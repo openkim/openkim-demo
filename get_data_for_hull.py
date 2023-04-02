962c9b6e5ea6122a1b03cb69439134d34d821053
ilia Nikiforov <nikif002@umn.edu>
2023-04-01 23:27:51 -0500

    firsty attempt docker

--- get_data_for_hull.py
+++ get_data_for_hull.py
@@ -1,9 +1,31 @@
 import requests
 
-def get_trs_for_hull(model,species_list):
-    result = requests.post("https://query.openkim.org/api",data={'query': '{"meta.type":"tr","meta.subject.extended-id":"%s","property-id":"tag:staff@noreply.openkim.org,2023-02-21:property/binding-energy-crystal","stoichiometric-species.source-value":{"$in":%s}}'%(model,str(species_list).replace("'",'"')), 'fields': '{"prototype-label.source-value":1,"stoichiometric-species.source-value":1,"binding-potential-energy-per-formula.source-value":1}', 'database': 'data'}).json()
+def get_formation_energies(species_list,model=None):
+    """
+    Query OpenKIM repository for test results or reference data for constructing a thermodynamic hull.
+    Args:
+        species_list:
+            List of all species to query for. Every result or data containing any combination of one or more of these will be returned
+        model:
+            OpenKIM model to query for. If this is None, reference data are queried for instead
+
+    Returns:
+        List of dicts, each corresponding to an OpenKIM test relaxation or reference data
+
+            element-fractions: list of molar fractions of each element in species_list in this material, must sum to 1.
+            formation-energy-per-atom: potential energy per atom relative to the crystalline ground states of each constituent element
+            prototype-label: AFLOW prototype label
+    """
+    assert len(species_list>1)
+    if model==None:
+        model_query=''
+        meta_type='rd'
+    else:
+        model_query='"meta.subject.extended-id":"%s"'%model
+        meta_type='tr'
+    result = requests.post("https://query.openkim.org/api",data={'query': '{"meta.type":"%s",%s,"property-id":"tag:staff@noreply.openkim.org,2023-02-21:property/binding-energy-crystal","stoichiometric-species.source-value":{"$in":%s}}'%(meta_type,model_query,str(species_list).replace("'",'"')), 'fields': '{"prototype-label.source-value":1,"stoichiometric-species.source-value":1,"binding-potential-energy-per-formula.source-value":1}', 'database': 'data'}).json()
     elemental_references = {}
-    # initialize dict for elemental reference
+    # initialize dict for elemental references
     for species in species_list:
         elemental_references[species]=0.
     for entry in result:
@@ -13,7 +35,12 @@ def get_trs_for_hull(model,species_list):
             species = species[0]
             if energy < elemental_references[species]:
                 elemental_references[species] = energy
-    # format of a 
     print(elemental_references)
-            
+    #now build the hull list of dicts
+    #each entry:
+    # array of fractions of each element
+    # formation energy per atom
+    # prototype label
+    
+
             
